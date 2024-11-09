4773674cf15e2cd0ab9482653f6e359169148bfb
muzaffarovhayotbek <hayotbek0511@gmail.com>
2024-11-09 14:46:08 +0500

    end

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('.btn button');
+const firstInput = document.querySelector('.first-input');
+const twoInput = document.querySelector('.two-input');
+
+function validate(firstInput, twoInput) {
+  if (twoInput.value !== '123456') {
+    alert('password no`g`ri "123456" bo`lishi kerak');
+    return false;
+  }
+  return true;
+}
+
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const isValid = validate(firstInput, twoInput);
+  if (!isValid) {
+    return;
+  }
+
+  window.location.href = 'pages/home.html';
+});
